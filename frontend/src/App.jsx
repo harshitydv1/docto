084e1af3106583ebe2835f47b5aac9150ec04029
harshitydv1 <harshit.raj@adypu.edu.in>
2026-02-03 23:19:42 +0530

    Add frontend files to repository

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Collection from './pages/Collection.jsx'
+import Product from './pages/Product.jsx'
+import Cart from './pages/Cart.jsx'
+import Login from './pages/Login.jsx'
+import Orders from './pages/Orders.jsx'
+import PlaceOrders from './pages/PlaceOrder.jsx'
+import Navbar from './components/Navbar.jsx'
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-orders" element={<PlaceOrders />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App
---
+++ frontend/src/pages/PlaceOrder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlaceOrder = () => {
+	return (
+		<div>Place Order</div>
+	)
+}
+
+export default PlaceOrder
