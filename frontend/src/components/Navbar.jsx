d7101644013ca4b74f8511a95fc028068aab6757
harshitydv1 <harshit.raj@adypu.edu.in>
2026-02-04 00:12:42 +0530

    checkpoint3

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { assets } from "../assets/assets";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -29,12 +29,25 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className="flex item-center gap-6">
-        <img src={assets.search_icon} className="w-5 cursor-pointer" alt=""  />
-        <div className="relative"></div>
-          <img src={assets.profile_icon} className="w-5 cursor-pointer" alt="" />
-          <div className="group-hover: block hidden absolute dropdown-menu right-0 pt-4"> 
-            
+        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+
+        <div className="group relative">
+          <img
+            className="w-5 cursor-pointer"
+            src={assets.profile_icon}
+            alt=""
+          />
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
           </div>
+        </div>
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} className="w-5 min-w-5" alt=""/>
+        </Link>
       </div>
     </div>
   );
