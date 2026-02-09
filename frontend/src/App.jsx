da303e46640ad62a3868de3a5247004e6e50bb59
harshitydv1 <harshit.raj@adypu.edu.in>
2026-02-10 01:25:55 +0530

    product image added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,13 +11,14 @@ import Orders from './pages/Orders.jsx'
 import PlaceOrders from './pages/PlaceOrder.jsx'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
+import SearchBar from './components/SearchBar.jsx'
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
 
       <Navbar />
-
+      <SearchBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 
 function Collection() {
-  const { products } = useContext(ShopContext);
+  const { products ,search , showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProduct, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -30,6 +30,11 @@ function Collection() {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+
+    if (showSearch && search){
+      productsCopy = productsCopy.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
+
+    }
 
     if (category.length > 0) {
       productsCopy = productsCopy.filter((item) =>
@@ -66,7 +71,7 @@ function Collection() {
 
   useEffect(() => {
     applyFilter();
-  }, [category, subCategory]);
+  }, [category, subCategory, search, showSearch]);
 
   useEffect(() => {
     sortProduct();
