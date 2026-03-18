0887149eccb20316df7a4e1ae438c7efed9649a8
harshitydv1 <harshit.raj@adypu.edu.in>
2026-03-18 12:30:16 +0530

    upload images fun

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -4,22 +4,26 @@ import { useState } from "react";
 import { backendUrl } from "../App";
 import { ToastContainer, toast } from "react-toastify";
 
-const Login = ({setToken}) => {
+const Login = ({ setToken }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const onSubmitHandler = async (e) => {
     try {
       e.preventDefault();
-      const response = await axios.post(backendUrl + '/api/user/admin',{email,password});
-      if(response.data.success){
+      const response = await axios.post(backendUrl + "/api/user/admin", {
+        email,
+        password,
+      });
+      if (response.data.success) {
         setToken(response.data.token);
-      }else{
-
+      } else {
+        toast.error(response.data.message);
       }
-
-
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
   };
 
   return (
@@ -28,7 +32,7 @@ const Login = ({setToken}) => {
         <h1 className="text-2xl font-bold mb-4">Admin Login</h1>
         <form onSubmit={onSubmitHandler}>
           <div className="mb-3 min-w-72">
-            <p className="text-sm font-medium text-grey-700 mb-2">
+            <p className="text-sm font-medium text-gray-700 mb-2">
               Email Address
             </p>
             <input
@@ -41,7 +45,7 @@ const Login = ({setToken}) => {
             />
           </div>
           <div className="mb-3 min-w-72">
-            <p className="text-sm font-medium text-grey-700 mb-2">Password</p>
+            <p className="text-sm font-medium text-gray-700 mb-2">Password</p>
             <input
               onChange={(e) => setPassword(e.target.value)}
               value={password}
@@ -52,7 +56,7 @@ const Login = ({setToken}) => {
             />
           </div>
           <button
-            className="mt-2 w-full py-2 px-4 rounded-md text-white bg-neutral-950 px-5 py-2.5 text-white duration-300 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:translate-y-1 active:scale-x-110 active:scale-y-90"
+            className="cursor-pointer mt-2 w-full py-2.5 px-5 rounded-md text-white bg-neutral-950 duration-300 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:translate-y-1 active:scale-x-110 active:scale-y-90"
             type="submit"
           >
             Login
--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Add = () => {
   return (
-    <div>
-        
-    </div>
-  )
+    <form>
+      <div>
+        <p>Upload Image</p>
+        <div>
+          <label htmlFor="image1">
+            <img src={assets.upload_area} alt="" />
+            <input type="file" id="image1" hidden />
+          </label>
+
+          <label htmlFor="image2">
+            <img src={assets.upload_area} alt="" />
+            <input type="file" id="image2" hidden />
+          </label>
+
+          <label htmlFor="image3">
+            <img src={assets.upload_area} alt="" />
+            <input type="file" id="image3" hidden />
+          </label>
+
+          <label htmlFor="image4">
+            <img src={assets.upload_area} alt="" />
+            <input type="file" id="image4" hidden />
+          </label>
+
+        </div>
+      </div>
+    </form>
+  );
 }
 
 export default Add
